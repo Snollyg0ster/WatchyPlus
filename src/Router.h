30a6fb425b0ad5c42702a5420f8b712864a2c94c
snolly <notyourbusiness@example.com>
2025-05-11 22:08:31 +0700

    add router

---
+++ src/Router.h
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <map>
+
+struct Route
+{
+    std::string name;
+    std::vector<std::string> routes;
+};
+
+class Router
+{
+private:
+    std::vector<Route> history;
+    std::map<std::string, Route> routeMap;
+
+public:
+    Router(std::vector<Route> routes);
+    Router(std::vector<Route> routes, std::string defaultRoute);
+    void setRoute(std::string name, bool clear);
+    Route getRoute();
+    bool back();
+};
