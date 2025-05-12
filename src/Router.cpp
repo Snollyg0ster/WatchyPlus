545a1ac20d3a1677a8363e65e3457fbbd78df3ed
snolly <you@example.com>
2025-05-12 22:55:31 +0700

    fix-router-inline-to-external

--- src/Router.cpp
+++ src/Router.cpp
@@ -1,39 +1,32 @@
 #include <Router.h>
 
-class Router {
-private:
-  std::vector<Route> history;
-  std::map<std::string, Route> routeMap;
+Router::Router(std::vector<Route> routes) {
+  for (Route r : routes) {
+    routeMap[r.name] = r;
+  }
+}
 
-public:
-  Router(std::vector<Route> routes) {
-    for (Route r : routes) {
-      routeMap[r.name] = r;
-    }
+void Router::setRoute(std::string name, bool clear = false) {
+  if (clear)
+    history.clear();
+  history.push_back(routeMap.at(name));
+}
+
+Router::Router(std::vector<Route> routes, std::string defaultRoute) {
+  for (Route r : routes) {
+    routeMap[r.name] = r;
   }
 
-  Router(std::vector<Route> routes, std::string defaultRoute) {
-    for (Route r : routes) {
-      routeMap[r.name] = r;
-    }
+  setRoute(defaultRoute);
+}
 
-    setRoute(defaultRoute);
+Route Router::getRoute() { return history.back(); }
+
+bool Router::back() {
+  if (history.size() < 2) {
+    return false;
   }
 
-  void setRoute(std::string name, bool clear = false) {
-    if (clear)
-      history.clear();
-    history.push_back(routeMap.at(name));
-  }
-
-  Route getRoute() { return history.back(); }
-
-  bool back() {
-    if (history.size() < 2) {
-      return false;
-    }
-
-    history.pop_back();
-    return true;
-  }
-};
+  history.pop_back();
+  return true;
+}
